37322277e8ce23ae2982635c45e26a249b353eb1
DevFadul <75513829+DevFadul@users.noreply.github.com>
2026-03-31 00:13:39 +0800

    add functions that clean and read data for manipulation

--- main/main.py
+++ main/main.py
@@ -1,3 +1,16 @@
 import pandas as pd
 
-print("Environment Ready 🚀")
+def read_data(file_path):
+    df = pd.read_csv(file_path)
+    return df
+
+def clean_data(df):
+     df['posting_date'] = pd.to_datetime(df['posting_date'])
+     df['application_deadline'] = pd.to_datetime(df['application_deadline'])
+     return df
+
+
+
+df = read_data('../data/ai_job_dataset.csv')
+df = clean_data(df)
+
